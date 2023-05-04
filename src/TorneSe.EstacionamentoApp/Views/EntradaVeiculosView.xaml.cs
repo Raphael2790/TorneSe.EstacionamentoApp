0fb7f027d35e86c1290c869cb8b70839a287b448
Raphael Silvestre dos Santos <raphael.silvestre-ext@viavarejo.com.br>
2023-05-03 22:21:36 -0300

    Criação de cards dinamicamente e componentização de grid para vagas - 03-05-2023

--- src/TorneSe.EstacionamentoApp/Views/EntradaVeiculosView.xaml.cs
+++ src/TorneSe.EstacionamentoApp/Views/EntradaVeiculosView.xaml.cs
@@ -1,16 +1,58 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using TorneSe.EstacionamentoApp.Componentes;
 using TorneSe.EstacionamentoApp.Store;
 
-namespace TorneSe.EstacionamentoApp.Views
+namespace TorneSe.EstacionamentoApp.Views;
+
+/// <summary>
+/// Interação lógica para EntradaVeiculosView.xam
+/// </summary>
+public partial class EntradaVeiculosView : UserControl
 {
-    /// <summary>
-    /// Interação lógica para EntradaVeiculosView.xam
-    /// </summary>
-    public partial class EntradaVeiculosView : UserControl
+    private readonly VeiculosStore _veiculosStore;
+    private int _pagina = 1;
+    private const int _paginaInicial = 1;
+    private int _porPagina = 20;
+    private double _totalPaginas = 0;
+
+    private const string _componente = "Entrada";
+
+    public EntradaVeiculosView(VeiculosStore veiculosStore)
     {
-        public EntradaVeiculosView(VeiculosStore veiculosStore)
+        InitializeComponent();
+        _veiculosStore = veiculosStore;
+        _totalPaginas = Math.Ceiling((double)(_veiculosStore.VagasLivres.Count / _porPagina));
+        MontarComponente();
+    }
+
+    private void MontarComponente()
+    {
+        var vagas = _veiculosStore.VagasLivres.Skip((_pagina - 1) * _porPagina).Take(_porPagina).ToList();
+
+        vagasControl.Content = new VagasGridControl(vagas, _componente);
+    }
+
+    private void ProximasVagas_ButtonClick(object sender, System.Windows.RoutedEventArgs e)
+    {
+        if (_pagina == _totalPaginas)
         {
-            InitializeComponent();
+            MessageBox.Show("Não há mais vagas a serem carregadas");
+            return;
         }
+
+        _pagina += 1;
+        MontarComponente();
+    }
+
+    private void VoltarVagas_ButtonClick(object sender, RoutedEventArgs e)
+    {
+        if (_pagina is _paginaInicial)
+            return;
+        
+        _pagina -= 1;
+        MontarComponente();
     }
 }
